574994f26ff74cb1e0667ee6b2ffce1c2542978b
loic.postec <loic.postec@heig-vd.ch>
2019-06-05 16:03:13 +0200

    change product page card style

--- resources/js/components/CarteProduit.vue
+++ resources/js/components/CarteProduit.vue
@@ -1,15 +1,20 @@
 <template>  
 <div class="container">
 <div class="card card-custom" v-for="vin in vins" :key=vin.id> 
+       <div class="container">
+
      <a v-bind:href="'produit/'+ vin.id">   
     <img src="/public/images/bottle.png" class="card-img-top" alt="vin1">
     </a>
-    <div class="card-body" id="weekproduct">
-    <h6 class="card-title">{{vin.nom}}</h6>
-      <p class="card-text">{{vin.millesime}}</p>
-      <p class="card-text">{{vin.produ.nom}}</p>
-      <p class="card-text">{{vin.prix.prixht}} CHF</p>
-      
+    </div>
+    <div class="container" id="carteproduit">
+    <h6 class="card-title" id="nom-carte">{{vin.nom}}</h6>
+    <hr>
+    <div class="card-body">
+      <h6  class="card-text" id="millesime-carte">{{vin.millesime}}</h6>
+      <h6  class="card-text" id="produ-carte">{{vin.produ.nom}}</h6>
+      <h6  class="card-text" id="prix-carte">CHF {{vin.prix.prixht}} </h6>
+      </div>
     </div>
   </div>
   </div>
