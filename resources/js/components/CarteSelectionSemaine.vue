f35d959d124c780ee2ab09290668583550913c1d
loic.postec <loic.postec@heig-vd.ch>
2019-06-06 17:15:04 +0200

    re-fix vue

---
+++ resources/js/components/CarteSelectionSemaine.vue
@@ -0,0 +1,38 @@
+<template>  
+<div class="container" >
+      <div class="card card-custom" v-for="vin in vins.slice(0,3)" :key=vin.id>
+               <div class="container">
+
+        <a v-bind:href="'produit/'+ vin.id"> 
+    <img src="images/illustrations/bottle.png" class="card-img-top" alt="vin1">
+    </a>
+    </div>
+    <div class="container" id="carteproduit">
+   
+      <div v-for="value in vin.types">
+         <h6 class="card-title" id="nom-carte">
+      <svg height="20" width="20">
+       <circle v-bind:class="'type-circle-'+ value.id" cx="10" cy="10" r="5" stroke="black" stroke-width="0.3"  />
+       </svg>
+
+       {{vin.nom}}</h6>
+              </div>
+    <div class="card-body">
+      <h6  class="card-text" id="millesime-carte">{{vin.millesime}}</h6>
+    </div>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['vins'],
+
+    data(){
+        return{
+          vin:'',
+        }
+     },
+}
+</script>
+
