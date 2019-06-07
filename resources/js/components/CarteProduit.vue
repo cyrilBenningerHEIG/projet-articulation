c962cd2e0612c98f1a65c4fe1308a55d1eddf429
Audris Chen <audris.chen@heig-vd.ch>
2019-06-07 14:57:51 +0200

    Selection des produits + Carte des produits

--- resources/js/components/CarteProduit.vue
+++ resources/js/components/CarteProduit.vue
@@ -10,13 +10,13 @@
       <div v-for="value in vin.types">
          <h6 class="card-title" id="nom-carte">
               <div class="container p-0">
-                <div class="row">
-                  <div class="col-1 pl-1 pr-1 m-0">
+                <div class="row" id="title-position">
+                  <div class="col-1 pr-2 pl-2">
                       <svg height="20" width="20">
                         <circle v-bind:class="'type-circle-'+ value.id" cx="10" cy="10" r="5" stroke="black" stroke-width="0.3"  />
                       </svg>
                   </div>
-                  <div class="col-11 pr-1 pl-1 vin-titre">
+                  <div class="col pr-2 pl-2vin-titre">
                         <h6>{{vin.nom}}</h6>
                   </div>
                 </div>
