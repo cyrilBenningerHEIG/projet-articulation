9367a57b2f73b9300d5e30d2e34d504ab4123f0a
Evan Vecchio <evan.vecchio@heig-vd.ch>
2019-06-11 10:08:07 +0200

    gaga

--- resources/js/components/Panier.vue
+++ resources/js/components/Panier.vue
@@ -76,27 +76,28 @@
           <td ></td>
           <td></td>
           <td>Sous total</td>
-          <td>{{totalCart}}</td>
+          <td>{{Math.round(totalCart * 10) / 10 }}</td>
           </tr>
           <tr>
           <td></td>
           <td></td>
           <td>TVA & autres taxes</td>
-          <td>{{totalCart*0.077}}</td>
+          <td>{{Math.round((totalCart*0.077 * 10)) / 10 }}</td>
           </tr>
            <tr>
           <td></td>
           <td></td>
           <td><h4>Total de la commande</h4><br>(Hors frais de livraison)</td>
-          <td><h4>{{(totalCart*0.077)+totalCart}}</h4></td>
+          <td><h4>{{Math.round(((totalCart*0.077)+totalCart) * 10) / 10 }}</h4></td>
           </tr>
         </tbody>
       </table>
       <div class="action-btn">
     <div class="row mt-5 justify-content-center">
       <div class="filters mx-auto">
-     <a href="/"><button class="btn btn-white btn-panier-2">Continuer mes achats</button></a>
-        <a href="paiement-etape1"><button class="btn btn-danger btn-panier">Passer commande</button></a>
+      <button class="btn btn-white btn-panier-2">Continuer mes achats</button>
+        <a v-if="userIsLoggedIn == false" href="paiement-etape1"><button class="btn btn-danger btn-panier">Passer commande</button></a>
+        <a v-else href="paiement-etape2"><button class="btn btn-danger btn-panier">Passer commande</button></a>
     </div>
 </div>
   </div>
@@ -116,6 +117,7 @@
 </template>
 
 <script>
+
 
 export default {
 
